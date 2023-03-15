1ad5d3be384610a75d2201ac04b99095352b9108
ekta2608 <112929206+ekta2608@users.noreply.github.com>
2023-03-15 17:33:41 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+simple interest.c
+#include<stdio.h>
+void main()
+{
+float p,r,t,s;
+printf("enter principle:");
+scanf("%f",&p);
+printf("\nenter rate:");
+scanf("%f",&r);
+printf("\nenter time:");
+scanf("%f",&t);
+s=p*r*t/100;
+printf("\nsimple interest":s);
+
+
+
+}
